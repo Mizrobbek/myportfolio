0303421de561e5f09ed1275f5c9c3b6dd794d7e8
Mizrobbek <baqoyevmizrob@gmail.com>
2023-09-12 16:42:10 +0500

    add js buttons

--- js/index.js
+++ js/index.js
@@ -1,10 +1,25 @@
-var elTheme = document.getElementById('theme');
-var elBody = document.getElementById('body');
+let elTheme = document.getElementById("theme");
+let elBody = document.getElementById("body");
+let header = document.querySelector(".header");
+let menuBtn = document.querySelector(".header__menu");
+let closeBtn = document.querySelector(".header__close");
 
-elTheme.addEventListener('change', function () {
+elTheme.addEventListener("change", function () {
   if (elTheme.checked === true) {
-    elBody.className = 'dark';
+    elBody.className = "dark";
   } else {
-    elBody.className = '';
+    elBody.className = "";
   }
+});
+
+menuBtn.addEventListener("click", () => {
+  header.classList.add("visual");
+  menuBtn.classList.add("hidden");
+  closeBtn.classList.add("visual");
+});
+
+closeBtn.addEventListener("click", () => {
+  header.classList.remove("visual");
+  menuBtn.classList.remove("hidden");
+  closeBtn.classList.remove("visual");
 });
